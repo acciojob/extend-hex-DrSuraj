4bd616b91349c05b9b6a41833855341b7cc1b541
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 15:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+    // Remove the # if present and convert to uppercase
+    shortHex = shortHex.replace("#", "").toUpperCase();
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+    // Extend the short hex code to a full hex code
+    let fullHex = '';
+    for (let i = 0; i < shortHex.length; i++) {
+        fullHex += shortHex[i].repeat(2);
+    }
+
+    // Add the # prefix and return the full hex code
+    return `#${fullHex}`;
+}
+
+// Examples
+console.log(extendHex("#abc")); // expected output: "#aabbcc"
+console.log(extendHex("abc"));  // expected output: "#aabbcc"
+console.log(extendHex("#AbC")); // expected output: "#AABBCC"
+console.log(extendHex("#f09")); // expected output: "#ff0099"
